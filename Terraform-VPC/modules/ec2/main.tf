9fd2ffd86b49f0a9a2412348b997d3ced9816bf3
pavantocemca40 <pavan.tocemca40@gmail.com>
2024-07-26 16:47:45 +0530

    changing instacne type

--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
     count = length(var.ec2_names)
     ami = data.aws_ami.amazon-2.id
-    instance_type = "t2.medium"
+    instance_type = "t2.micro"
     associate_public_ip_address = true
     vpc_security_group_ids = [var.sg_id]
     subnet_id = var.subnets[count.index]
